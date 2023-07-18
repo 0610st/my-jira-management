63224f545087951bf40a5514ca9ecc537abcdb80
commiter <88723518+0610st@users.noreply.github.com>
2023-07-18 23:55:28 +0900

    generate open-api json

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { generateOpenApi } from '@ts-rest/open-api';
 import { api } from 'contracts';
 import { SwaggerModule } from '@nestjs/swagger';
+import { writeFileSync } from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +18,10 @@ async function bootstrap() {
       version: '1.0.0',
     },
   });
+  writeFileSync(
+    '../../docs/swagger.json',
+    JSON.stringify(document, undefined, 2),
+  );
   SwaggerModule.setup('api', app, document);
 
   await app.listen(3001);
